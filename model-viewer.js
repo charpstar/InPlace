6b688385e67ddcf80ff6f6c2a043ed6024b58685
charpstar <35925783+charpstar@users.noreply.github.com>
2020-08-28 11:08:53 +0530

    Add files via upload

--- model-viewer.js
+++ model-viewer.js
@@ -59095,7 +59095,7 @@ const openIOSARQuickLook = (() => {
     if (document.getElementById("inplace-viewer")) {
     anchor.setAttribute("id", "charpstar");
  //Add domain where you want Add to Cart
-  if ((document.domain == "charpstar.se" || document.domain == "trademax.se" || document.domain == "trademax.fi" || document.domain == "trademax.no" || document.domain == "trademax.dk" || document.domain == "chilli.se" || document.domain == "chilli.no" || document.domain == "furniturebox.se" || document.domain == "furniturebox.no" ) && document.querySelector('[data-ar="product-name"]')) {
+  if ((document.domain == "charpstar.se" || document.domain == "trademax.se" || document.domain == "trademax.fi" || document.domain == "trademax.no" || document.domain == "trademax.dk" || document.domain == "chilli.se" || document.domain == "chilli.no" || document.domain == "furniturebox.se" || document.domain == "furniturebox.no" || document.domain == "www.trademax.se" || document.domain == "www.trademax.fi" || document.domain == "www.trademax.no" || document.domain == "www.trademax.dk" || document.domain == "www.chilli.se" || document.domain == "www.chilli.no" || document.domain == "www.furniturebox.se" || document.domain == "www.furniturebox.no") && document.querySelector('[data-ar="product-name"]')) {
     var productTitle =  document.querySelector('[data-ar="product-name"]').innerHTML;
     var productCurrentPrice =  document.querySelector('[data-ar="current-price"]').innerHTML;
     var domainName = document.querySelector('meta[property="og:site_name"]').content; 
@@ -62987,7 +62987,7 @@ const FocusVisiblePolyfillMixin = (SuperClass) => {
 
 window.addEventListener("message", function (event) {   
      if (event.data == "_apple_ar_quicklook_button_tapped") {
-         if (document.domain == "trademax.se" || document.domain == "trademax.fi" || document.domain == "trademax.no" || document.domain == "trademax.dk" || document.domain == "chilli.se" || document.domain == "chilli.no" || document.domain == "furniturebox.se" || document.domain == "furniturebox.no")
+         if (document.domain == "trademax.se" || document.domain == "trademax.fi" || document.domain == "trademax.no" || document.domain == "trademax.dk" || document.domain == "chilli.se" || document.domain == "chilli.no" || document.domain == "furniturebox.se" || document.domain == "furniturebox.no" || document.domain == "www.trademax.se" || document.domain == "www.trademax.fi" || document.domain == "www.trademax.no" || document.domain == "www.trademax.dk" || document.domain == "www.chilli.se" || document.domain == "www.chilli.no" || document.domain == "www.furniturebox.se" || document.domain == "www.furniturebox.no")
          {
         document.querySelector('[data-ar="add-to-cart"]').click();
          }
