23289fb65c4a42c8b4a6108fa29c5c32780b5d34
charpstar <35925783+charpstar@users.noreply.github.com>
2020-10-15 08:39:54 +0200

    Add files via upload

---
+++ charpstar-viewer.js
@@ -0,0 +1,55 @@
+const openIOSARQuickLook = (() => {
+    const anchor = document.createElement('a');
+    anchor.setAttribute('rel', 'ar');
+    anchor.appendChild(document.createElement('img'));
+    anchor.setAttribute("id", "charpstar");
+       return (usdzSrc) => {
+        anchor.setAttribute('href', usdzSrc);
+        anchor.click();
+       };
+})();
+
+const openSceneViewer = (() => {
+    const anchor = document.createElement('a');
+    const noArViewerSigil = '#model-viewer-no-ar-fallback';
+    let fallbackInvoked = false;
+    return (gltfSrc, title, arScale) => {
+        // If the fallback has ever been invoked this session, bounce early:
+        if (fallbackInvoked) {
+            return;
+        }
+        
+        const location = self.location.toString();
+        const locationUrl = new URL(location);
+        const modelUrl = new URL(gltfSrc, location);
+        const scheme = modelUrl.protocol.replace(':', '');
+        locationUrl.hash = noArViewerSigil;
+        let intentParams = `?file=${encodeURIComponent(modelUrl.toString())}&mode=3d_preferred&link=${location}&title=${encodeURIComponent(document.title)}`;
+        if (arScale === 'fixed') {
+            intentParams += `&resizable=false`;
+        }
+        const intent = `intent://arvr.google.com/scene-viewer/1.0${intentParams}#Intent;scheme=${scheme};package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`;
+        const undoHashChange = () => {
+            if (self.location.hash === noArViewerSigil && !fallbackInvoked) {
+                fallbackInvoked = true;
+                self.history.back();
+            }
+        };
+        self.addEventListener('hashchange', undoHashChange, { once: true });
+        anchor.setAttribute('href', intent);
+        anchor.click();
+    };
+})();
+
+
+function activateAR(e) {
+     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+     if (/android/i.test(userAgent)) {
+          openSceneViewer(e.getAttribute("data-androidSrc"));
+    }
+    else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+     openIOSARQuickLook(e.getAttribute("data-iosSrc"));
+    }
+}
+
+ 
