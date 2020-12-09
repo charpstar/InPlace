7a99f384413662dc4d57b011b1753d43078a0a07
charpstar <35925783+charpstar@users.noreply.github.com>
2020-12-09 12:11:20 +0100

    Add files via upload

--- charpstar-viewer.js
+++ charpstar-viewer.js
@@ -1,23 +1,24 @@
 window.onload = function() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const ARClass = urlParams.get('ARClass');
+    if (ARClass !== null) {
+        activateAR(document.getElementsByClassName(ARClass)[0]);
+    }
     
-if (document.domain != "charpstar.se"){
-checkForARSupport();
-checkForiOSSupport();
-}
+    checkForARSupport();
     
 }
-
 const openIOSARQuickLook = (() => {
     const anchor = document.createElement('a');
     anchor.setAttribute('rel', 'ar');
     anchor.appendChild(document.createElement('img'));
     anchor.setAttribute("id", "charpstar");
-       return (usdzSrc) => {
+    return (usdzSrc) => {
         anchor.setAttribute('href', usdzSrc);
         anchor.click();
-       };
+    };
 })();
-
 const openSceneViewer = (() => {
     const anchor = document.createElement('a');
     const noArViewerSigil = '#model-viewer-no-ar-fallback';
@@ -27,7 +28,6 @@ const openSceneViewer = (() => {
         if (fallbackInvoked) {
             return;
         }
-        
         const location = self.location.toString();
         const locationUrl = new URL(location);
         const modelUrl = new URL(gltfSrc, location);
@@ -37,77 +37,161 @@ const openSceneViewer = (() => {
         if (arScale === 'fixed') {
             intentParams += `&resizable=false`;
         }
-        const intent = `intent://arvr.google.com/scene-viewer/1.0${intentParams}#Intent;scheme=${scheme};package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`;
+         const intent = `intent://arvr.google.com/scene-viewer/1.0${intentParams}#Intent;scheme=${scheme};package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=${encodeURIComponent(locationUrl.toString())};end;`;
         const undoHashChange = () => {
             if (self.location.hash === noArViewerSigil && !fallbackInvoked) {
                 fallbackInvoked = true;
                 self.history.back();
             }
         };
-        self.addEventListener('hashchange', undoHashChange, { once: true });
+        self.addEventListener('hashchange', undoHashChange, {
+            once: true
+        });
         anchor.setAttribute('href', intent);
         anchor.click();
     };
 })();
 
-
 function activateAR(e) {
-     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-     if (/android/i.test(userAgent)) {
-          openSceneViewer(e.getAttribute("data-androidSrc"));
-    }
-    else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-     openIOSARQuickLook(e.getAttribute("data-iosSrc"));
+    
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/android/i.test(userAgent)) {
+        openSceneViewer(e.getAttribute("data-androidSrc"));
+    } else if (/iPad|iPhone|iPod/.test(userAgent) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) {
+        openIOSARQuickLook(e.getAttribute("data-iosSrc"));
+    } else {
+     //QR Code Toggle on Desktops
+        toggleModal();
     }
 }
 
 function checkForiOSSupport() {
-	var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-	// check for AR support
-	const a = document.createElement('a'),
-		  supportsAR = a.relList.supports('ar') ? true : false;
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream &&  !supportsAR) {
-          var x = document.getElementsByClassName("charpstarARViewer");
-        var i;
-         for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";
-  }
-        
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    var userAgentSafari = window.navigator.userAgent.toLowerCase(),
+    safari = /safari/.test(userAgentSafari)
+    // check for AR support
+    const a = document.createElement('a'),
+        supportsAR = a.relList.supports('ar') ? true : false;
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream && !supportsAR) {
+        hideARButton();
     }
+    if (!safari){
+            hideARButton();
+    }
+
     
 }
 
+function checkForAndroidSupport() {
+    var a = navigator.userAgent;
+    if ( a.includes ('wv')) {
+          hideARButton();
+    } 
+}
+
 function checkForARSupport() {
-
- var isMobile = {
-            Android: function () {
-                return navigator.userAgent.match(/Android/i);
-            },
-            iOS: function () {
-                return navigator.userAgent.match(/iPhone/i) ;
-            },
-            iPad: function(){
-                return (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
-            },
-            any: function () {
-                return (isMobile.Android() ||  isMobile.iOS() || isMobile.iPad());
-            }
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone/i);
+        },
+        iPad: function() {
+            return (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.iOS() || isMobile.iPad());
+        }
+    };
+    if (!isMobile.any()) {
+        //You can either Hide the AR Button Here for desktop devices or decide to show it. If Visible and When clicked, the button will pop up a QR code. 
+        //hideARButton();
+         var x = document.getElementsByClassName("charpstarARViewer");
+         var i;
+         for (i = 0; i < x.length; i++) {
+          if (!x[i].hasAttribute("data-desktopQR")) {
+               hideARButton();
+          }
          
-        };
-if (!isMobile.any()) {
-   var x = document.getElementsByClassName("charpstarARViewer");
-        var i;
-         for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";
-}
-}
-else {
-      var x = document.getElementsByClassName("charpstarARViewer");
-        var i;
-         for (i = 0; i < x.length; i++) {
-         x[i].style.display = "inline-block";
-}
+    }
+    } else if (isMobile.Android()) {
+      checkForAndroidSupport();
+    } else if (isMobile.iOS() || isMobile.iPad()) {
+     
+        checkForiOSSupport();
+    }
 }
 
+function hideARButton() {
+    if ( document.getElementsByClassName("charpstarARViewer")) {
+    var x = document.getElementsByClassName("charpstarARViewer");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    }
 }
- 
+
+
+
+
+//QR Code Logic 
+
+var QRCodeModalActive = false;
+var QRMainSrc = "https://charpstar.se/InPlace-Integration-Full/QRGen.html?charpstar-ar-href=";
+var QRCurrentProductAppend = window.location.href + "?ARClass=charpstarARViewer";
+var fullQRSrc = QRMainSrc + QRCurrentProductAppend;
+var QRModalDiv = document.createElement("div");
+QRModalDiv.setAttribute('class', 'QRModal');
+QRModalDiv.style.zIndex = "15";
+QRModalDiv.style.width = "100%";
+QRModalDiv.style.height = "100%";
+QRModalDiv.style.position = "fixed";
+QRModalDiv.style.background = "rgba(0, 0, 0, 0.5)";
+QRModalDiv.style.opacity = "1";
+QRModalDiv.style.visibility = "visible";
+QRModalDiv.style.transform = "scale(1.1)";
+QRModalDiv.style.transition = "visibility 0s linear 0.25s, opacity 0.25s 0s, transform 0.25s";
+QRModalDiv.style.top = "0";
+QRModalDiv.style.left = "0";
+var QRModaliframe = document.createElement('iframe');
+QRModaliframe.src = fullQRSrc;
+QRModaliframe.frameborder = "0";
+QRModaliframe.height = "585px";
+QRModaliframe.width = "404px";
+QRModaliframe.style.top = "50%";
+QRModaliframe.style.left = "50%";
+QRModaliframe.style.position = "absolute";
+QRModaliframe.style.transform = "translate(-50%, -50%)";
+QRModalDiv.appendChild(QRModaliframe);
+
+function toggleModal() {
+    if (QRCodeModalActive == false) {
+        document.body.appendChild(QRModalDiv);
+        setTimeout(function() {
+            QRCodeModalActive = true;
+        }, 200);
+    } else {
+        document.body.removeChild(QRModalDiv);
+        QRCodeModalActive = false;
+    }
+}
+
+function windowOnClick(event) {
+    if (QRCodeModalActive == true) {
+        toggleModal();
+    }
+}
+window.addEventListener("click", windowOnClick);
+
+function QRCloseButtonEvent(msg) {
+    if (msg.data == "CloseQRModal") {
+        toggleModal();
+    }
+}
+if (window.addEventListener) {
+    window.addEventListener("message", QRCloseButtonEvent, false);
+} else {
+    window.attachEvent("onmessage", QRCloseButtonEvent);
+}
